2dbd0d4349e55c3765079fc1712b3feb3a7e8b1b
greatbrain <erickfania@gmail.com>
2025-03-04 09:49:31 -0400

    add:footer social media

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,18 @@ window.addEventListener('scroll', () => {
     const banner = document.querySelector('.banner');
     const scrollPosition = window.scrollY;
     const bannerHeight = banner.offsetHeight;
-    const opacity = 1 - (scrollPosition / bannerHeight); // Calcula la opacidad
+    const opacity = 1 - (scrollPosition / bannerHeight);
 
-    if (opacity >= 0) { // Evita valores de opacidad negativos
+    if (opacity >= 0) {
         banner.style.opacity = opacity;
     } else {
-        banner.style.opacity = 0; // Asegura que la opacidad no sea menor que 0
+        banner.style.opacity = 0;
     }
 });
-
 
 /*Lazy load banner text */
 window.addEventListener('DOMContentLoaded', () => {
     const elements = document.querySelectorAll('.fade-in-element');
-
     elements.forEach(element => {
         setTimeout(() => {
             element.classList.add('show');
@@ -24,24 +22,20 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    const sliderContainer = document.getElementById('slider-container');
-    let slideIndex = 0;
-    const slides = sliderContainer.children;
-    const slideWidth = slides[0].offsetWidth;
-
-    function nextSlide() {
-        slideIndex++;
-        if (slideIndex >= slides.length) {
-            slideIndex = 0;
-        }
-        updateSlider();
-    }
-
-    function updateSlider() {
-        sliderContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
-    }
-
-    setInterval(nextSlide, 5000); // Cambia la imagen cada 3 segundos
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     const sliderContainer = document.getElementById('slider-container');
+//     let slideIndex = 0;
+//     const slides = sliderContainer.children;
+//     const slideWidth = slides[0].offsetWidth;
+//     function nextSlide() {
+//         slideIndex++;
+//         if (slideIndex >= slides.length) {
+//             slideIndex = 0;
+//         }
+//         updateSlider();
+//     }
+//     function updateSlider() {
+//         sliderContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
+//     }
+//     setInterval(nextSlide, 5000);
+// });
