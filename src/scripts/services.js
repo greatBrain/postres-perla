5cdd56980c787651af2ca76dccdfca8c2e17ce72
Erick Fana <erickFania@gmail.com>
2025-10-27 14:37:28 -0400

    ref:  changing pastry services

---
+++ src/scripts/services.js
@@ -0,0 +1,22 @@
+const ALL_SERVICES = [
+    {
+        serviceImgUrl: "./public/icons/sweet-table-service.svg",
+        serviceTitle: "Diseño de pasteles personalizados",        
+        serviceDescription: "Arte Comestible para tus Momentos Cumbres. El servicio incluye una sesión de consulta y diseño para definir sabor, relleno, terminación y estructura. Nos ajustamos a su paleta de colores y tema. Agenda tu degustación."
+    },
+    {
+        serviceImgUrl: "./public/icons/whole-cake-service.svg",
+        serviceTitle: "Montaje de estaciones de dulces",        
+        serviceDescription: "El Toque Dulce y Estilizado para tu Celebración. Ofrecemos la curación y montaje de mesas de postres para bodas, bautizos, o eventos corporativos. Incluye préstamo de bases y servicio de montaje/desmontaje."
+    },
+    {
+        serviceImgUrl: "./public/icons/desserts-service.svg",
+        serviceTitle: "Servicios de repostería corporativa",        
+        serviceDescription: "Detalles Dulces de Alto Impacto para tu Marca. Soluciones para eventos de negocios, coffee breaks o regalos para clientes. Ofrecemos logotipos comestibles, empaque individual profesional y facturación formal."
+    },
+    {
+        serviceImgUrl: "./public/icons/desserts-service.svg",
+        serviceTitle: "Muestras y kits de degustación",        
+        serviceDescription: "Prueba Antes de Celebrar en Grande. Reserva uno de nuestros Kits de Degustación. Recibirás una caja con muestras de nuestros bizcochos más populares y rellenos. El costo del kit es acreditado al total de tu pedido final."
+    },
+];
